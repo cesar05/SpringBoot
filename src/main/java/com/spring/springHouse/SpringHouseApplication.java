1023801be98920b02cc67c1ecf81853b9621fd97
César Muñoz <munozroldanc@gmail.com>
2018-08-03 22:30:25 -0500

    Estructura del proyecto

---
+++ src/main/java/com/spring/springHouse/SpringHouseApplication.java
@@ -0,0 +1,12 @@
+package com.spring.springHouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHouseApplication.class, args);
+	}
+}
